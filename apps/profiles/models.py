e8ec1ad11a98444dd40ddf291e183b050370eccf
Big Steppa <steppacodes@gmail.com>
2024-10-20 21:20:24 +0100

    chore: remove Request model[change of logic]

--- apps/profiles/models.py
+++ apps/profiles/models.py
@@ -14,6 +14,7 @@ class Insightor(BaseModel):
     available = models.BooleanField(default=True)
     hourly_rate = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     linkedin_url = models.URLField(blank=True, null=True)
+    resume = models.FileField(null=True, blank=True)
     facebook_url = models.URLField(blank=True, null=True)
     website_url = models.URLField(blank=True, null=True)
     
@@ -35,10 +36,3 @@ class Certifications(BaseModel):
     name = models.CharField(max_length=300)
     issuing_organization = models.CharField(max_length=300)
     issue_date = models.DateField()
-
-
-class Request(BaseModel):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    desc = models.TextField(_("description"))
-    terms_agreement = models.BooleanField(default=False)
-    resume = models.FileField()
